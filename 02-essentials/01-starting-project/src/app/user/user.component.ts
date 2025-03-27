37507f17b4df9befd625c7ecaa33cf36bc490b21
Gopall Borse <gopallborse@gmail.com>
2025-03-27 17:23:02 +0530

    input decorator based approach

--- 02-essentials/01-starting-project/src/app/user/user.component.ts
+++ 02-essentials/01-starting-project/src/app/user/user.component.ts
@@ -7,19 +7,12 @@ import { Component, computed, Input, input } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  // @Input({ required: true }) avatar!: string;
-  // @Input({ required: true }) name!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
 
-  avatar = input.required<string>();
-  name = input.required<string>();
-
-  // get imagePath() {
-  //   return 'assets/users/' + this.avatar;
-  // }
-
-  imagePath = computed(() => {
-    return 'assets/users/' + this.avatar();
-  });
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
 
   onSelectUser() {}
 }
